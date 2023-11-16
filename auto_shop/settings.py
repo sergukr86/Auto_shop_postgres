fab4dde4965ed34d9f9925cbeb46e628c9117254
sergukr86 <sergukr86@gmail.com>
2023-11-17 00:34:09 +0100

    try another db config

--- auto_shop/settings.py
+++ auto_shop/settings.py
@@ -116,13 +116,9 @@ if IS_HEROKU_APP:
     # environment variable by the dj-database-url package. `DATABASE_URL` will be set
     # automatically by Heroku when a database addon is attached to your Heroku app. See:
     # https://devcenter.heroku.com/articles/provisioning-heroku-postgres
-    # https://github.com/jazzband/dj-database-urlconn_health_checks
+    # https://github.com/jazzband/dj-database-url
     DATABASES = {
-        "default": dj_database_url.config(
-            conn_max_age=600,
-            conn_health_checks=True,
-            ssl_require=True,
-        ),
+        "default": dj_database_url.config(),
     }
 else:
     # When running locally in development or in CI, a sqlite database file will be used instead
